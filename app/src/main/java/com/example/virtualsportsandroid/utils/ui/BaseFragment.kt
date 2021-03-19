dab97bfc468cfbaac050c826f470e71060661449
HardSea <androidmen2000@gmail.com>
2021-03-19 19:24:44 +0200

    Add some TextView and strings

--- app/src/main/java/com/example/virtualsportsandroid/utils/ui/BaseFragment.kt
+++ app/src/main/java/com/example/virtualsportsandroid/utils/ui/BaseFragment.kt
@@ -1,5 +1,13 @@
 package com.example.virtualsportsandroid.utils.ui
 
 import androidx.fragment.app.Fragment
+import com.example.virtualsportsandroid.MainActivity
+import com.example.virtualsportsandroid.utils.FragmentNavigator
 
-abstract class BaseFragment(layoutId: Int) : Fragment(layoutId)
+abstract class BaseFragment(layoutId: Int) : Fragment(layoutId) {
+
+    protected val navigator: FragmentNavigator by lazy {
+        (requireActivity() as MainActivity).fragmentNavigator
+    }
+
+}
