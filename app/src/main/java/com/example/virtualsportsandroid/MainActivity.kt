232506c3d4c44088f960b9201bd739a213bd1991
HardSea <androidmen2000@gmail.com>
2021-03-22 22:02:16 +0200

    Hide keyboard on touch outside

--- app/src/main/java/com/example/virtualsportsandroid/MainActivity.kt
+++ app/src/main/java/com/example/virtualsportsandroid/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.virtualsportsandroid
 
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.virtualsportsandroid.game.data.ScreenGameModel
@@ -25,6 +28,15 @@ class MainActivity : AppCompatActivity() {
                 "", "", "https://vsw.betradar.com/ls/mobile/?/parimatchrgs/ru/page/vsmobile_demo"
             )
         )
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 
     override fun onBackPressed() {
