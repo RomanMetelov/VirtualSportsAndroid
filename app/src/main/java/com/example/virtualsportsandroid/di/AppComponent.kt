46c55034dcae085c540f5ea94aaf07df5299789b
HardSea <androidmen2000@gmail.com>
2021-03-22 12:16:10 +0200

    Create NetworkHelper

--- app/src/main/java/com/example/virtualsportsandroid/di/AppComponent.kt
+++ app/src/main/java/com/example/virtualsportsandroid/di/AppComponent.kt
@@ -5,6 +5,7 @@ package com.example.virtualsportsandroid.di
 import com.example.virtualsportsandroid.login.ui.LoginFragment
 import com.example.virtualsportsandroid.login.ui.RegistrationFragment
 import com.example.virtualsportsandroid.mainScreen.ui.MainFragment
+import com.example.virtualsportsandroid.nonetwork.NetworkErrorFragment
 import dagger.Component
 import javax.inject.Singleton
 
@@ -14,5 +15,6 @@ interface AppComponent {
 
     fun inject(registrationFragment: RegistrationFragment)
     fun inject(loginFragment: LoginFragment)
+    fun inject(networkErrorFragment: NetworkErrorFragment)
     fun inject(mainFragment: MainFragment)
 }
