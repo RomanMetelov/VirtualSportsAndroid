ad6bc12e7521e07ce1602df4cf88734dc7e0d158
Maxym Vakonyuk <makswk04@gmail.com>
2021-03-25 01:09:46 +0200

    added async loading not filtered games

--- app/src/main/java/com/example/virtualsportsandroid/games/domain/NotFilteredGamesLoadingUseCase.kt
+++ app/src/main/java/com/example/virtualsportsandroid/games/domain/NotFilteredGamesLoadingUseCase.kt
@@ -2,9 +2,11 @@ package com.example.virtualsportsandroid.games.domain
 
 import android.content.Context
 import com.example.virtualsportsandroid.R
-import com.example.virtualsportsandroid.games.data.GamesRepository
 import com.example.virtualsportsandroid.games.data.GamesLoadingError
+import com.example.virtualsportsandroid.games.data.GamesRepository
+import com.example.virtualsportsandroid.games.domain.model.TagModel
 import com.example.virtualsportsandroid.games.ui.GamesFragmentState
+import com.example.virtualsportsandroid.utils.Result
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
@@ -14,29 +16,35 @@ class NotFilteredGamesLoadingUseCase(
     private val context: Context
 ) {
     suspend operator fun invoke(): GamesFragmentState = withContext(dispatcher) {
-        val allGamesWithoutFirstTagResult = gamesRepository.getAllGamesWithoutFirstTag()
-        val gamesWithFirstTagResult = gamesRepository.getGamesWithFirstTag()
-        when {
-            allGamesWithoutFirstTagResult.isError -> {
-                when (allGamesWithoutFirstTagResult.errorResult) {
-                    GamesLoadingError.NOT_FOUND -> {
-                        return@withContext GamesFragmentState.Error(context.getString(R.string.not_found_message))
-                    }
+        handleResult(
+            withContext(dispatcher) { gamesRepository.getAllGamesWithoutFirstTag() },
+            withContext(dispatcher) { gamesRepository.getGamesWithFirstTag() }
+        )
+    }
+
+    private fun handleResult(
+        allGamesWithoutFirstTagResult: Result<List<TagModel>, GamesLoadingError>,
+        gamesWithFirstTagResult: Result<TagModel, GamesLoadingError>
+    ) = when {
+        allGamesWithoutFirstTagResult.isError -> {
+            when (allGamesWithoutFirstTagResult.errorResult) {
+                GamesLoadingError.NOT_FOUND -> {
+                    GamesFragmentState.Error(context.getString(R.string.not_found_message))
                 }
             }
-            gamesWithFirstTagResult.isError -> {
-                when (gamesWithFirstTagResult.errorResult) {
-                    GamesLoadingError.NOT_FOUND -> {
-                        return@withContext GamesFragmentState.Error(context.getString(R.string.not_found_message))
-                    }
+        }
+        gamesWithFirstTagResult.isError -> {
+            when (gamesWithFirstTagResult.errorResult) {
+                GamesLoadingError.NOT_FOUND -> {
+                    GamesFragmentState.Error(context.getString(R.string.not_found_message))
                 }
             }
-            else -> {
-                return@withContext GamesFragmentState.NotFiltered(
-                    gamesWithFirstTagResult.successResult,
-                    allGamesWithoutFirstTagResult.successResult
-                )
-            }
+        }
+        else -> {
+            GamesFragmentState.NotFiltered(
+                gamesWithFirstTagResult.successResult,
+                allGamesWithoutFirstTagResult.successResult
+            )
         }
     }
 }
