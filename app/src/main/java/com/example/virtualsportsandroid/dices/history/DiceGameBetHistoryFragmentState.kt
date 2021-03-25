c5f5fddb2c1715e6447d7486ed41f8ebb61436d1
Roman Metelov <rm0734861369@gmail.com>
2021-03-25 12:28:09 +0200

    tests fail issue fixed

--- app/src/main/java/com/example/virtualsportsandroid/dices/history/DiceGameBetHistoryFragmentState.kt
+++ app/src/main/java/com/example/virtualsportsandroid/dices/history/DiceGameBetHistoryFragmentState.kt
@@ -1,10 +1,6 @@
 package com.example.virtualsportsandroid.dices.history
 
-import com.example.virtualsportsandroid.dices.DiceGameBetHistoryModel
 import com.example.virtualsportsandroid.dices.DiceGameResultModel
-import com.example.virtualsportsandroid.filter.data.models.CategoryResponse
-import com.example.virtualsportsandroid.filter.data.models.ProviderResponse
-import com.example.virtualsportsandroid.filter.ui.FilterFragmentState
 
 sealed class DiceGameBetHistoryFragmentState {
     object Loading : DiceGameBetHistoryFragmentState()
--- app/src/main/java/com/example/virtualsportsandroid/dices/history/domain/DiceGameBetHistoryState.kt
+++ app/src/main/java/com/example/virtualsportsandroid/dices/history/domain/DiceGameBetHistoryState.kt
@@ -1,7 +1,5 @@
 package com.example.virtualsportsandroid.dices.history.domain
 
-import com.example.virtualsportsandroid.mainScreen.domain.model.GameModel
-
 sealed class DiceGameBetHistoryState {
     object Loading : DiceGameBetHistoryState()
     data class Error(val errorMessage: String) : DiceGameBetHistoryState()
