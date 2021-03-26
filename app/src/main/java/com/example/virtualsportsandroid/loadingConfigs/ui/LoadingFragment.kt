8e203f7162dda59c747a8af77f021f44042397ce
HardSea <androidmen2000@gmail.com>
2021-03-26 18:27:40 +0200

    Fix blink header

--- app/src/main/java/com/example/virtualsportsandroid/loadingConfigs/ui/LoadingFragment.kt
+++ app/src/main/java/com/example/virtualsportsandroid/loadingConfigs/ui/LoadingFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import com.example.virtualsportsandroid.Application
 import com.example.virtualsportsandroid.R
-import com.example.virtualsportsandroid.game.data.ScreenGameModel
 import com.example.virtualsportsandroid.utils.ui.BaseFragment
 import javax.inject.Inject
 
@@ -32,15 +31,15 @@ class LoadingFragment : BaseFragment(R.layout.loading_fragment) {
         viewModel.loadingStateLiveData.observe(viewLifecycleOwner) {
             if (it == ConfigsLoadingState.Successful) {
                 navigator.showMainFragment()
-                navigator.showLoginFragment()
-                navigator.showGameFragment(
-                    ScreenGameModel(
-                        "157",
-                        "Game test",
-                        "https://vsw.betradar.com/ls/mobile/?/parimatchrgs/ru/page/vsmobile_demo",
-
-                    )
-                )
+                
+//                navigator.showGameFragment(
+//                    ScreenGameModel(
+//                        "157",
+//                        "Game test",
+//                        "https://vsw.betradar.com/ls/mobile/?/parimatchrgs/ru/page/vsmobile_demo",
+//
+//                    )
+//                )
             }
         }
     }
